9c704924c203bba7809bf75dfef86489b5e33828
Jose Fernandez de Castro <jose.fernandez@flatironschool.com>
2023-01-26 23:08:15 -0500

    Changes

---
+++ test/test.sh
@@ -0,0 +1,8 @@
+echo "[Debug] Test Debug Output"
+echo "Debug: Test Debug Output"
+echo "[Error] Test Error Output"
+echo "Error: Test Error Output"
+
+RED='\033[0;31m'
+NC='\033[0m'
+printf "This is ${RED}TEST${NC} output"
