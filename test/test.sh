cd283855bd7814510a836621d991653cc501bc02
Jose Fernandez de Castro <jose.fernandez@flatironschool.com>
2023-01-26 23:39:34 -0500

    Changes

--- test/test.sh
+++ test/test.sh
@@ -3,4 +3,4 @@ echo "${B}This is TEST output"
 echo "${G}This is TEST output"
 echo "${Y}This is TEST output"
 touch ./results.txt
-echo "Test Results" >> results.txt
+echo "Test Results" >> results.csv
