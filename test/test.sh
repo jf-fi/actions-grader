58cbd11f803b2e2b2b9757c07c5bfcc8dcbfdd74
Jose Fernandez de Castro <jose.fernandez@flatironschool.com>
2023-01-27 21:47:42 -0500

    Test change

--- test/test.sh
+++ test/test.sh
@@ -1,6 +1,6 @@
 lg -r p -m "This one passed"
 lg -r f -m "This one failed"
-lg -r f -m "This one failed" -e "You messed up"
+lg -r f -m "This one failed"
 lg -r f -m "This one failed"
 lg -r p -m "This one passed"
 lg -o
