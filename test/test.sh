45d953a756c3ff6d1289ba702fa717f8f548bece
Jose Fernandez de Castro <jose.fernandez@flatironschool.com>
2023-01-31 01:19:01 -0500

    Test change

--- test/test.sh
+++ test/test.sh
@@ -1,2 +1,5 @@
 lg -r p -m "This one passed" -e "Test explanation"
-lg -r p -m "This one passed"
+lg -r p -m "This one also passed"
+lg -r f -m "This one failed" -e "This was wrong because X"
+lg -r p -m "This one passed too"
+lg -r f -m "This one also failed" -e "This was wrong because Y"
